3dc191495a165699fd7d0fc11d7ba0ecd73baa7e
Koteswararao95 <koteswararaokampasati@gmail.com>
2025-12-18 03:20:16 +0530

    Add food delivery application

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
  export const connectDB = async()=>{
-    await mongoose.connect('mongodb+srv://kampasatikampasati76:234@cluster0.4s1eubr.mongodb.net/FOOD-DEL')
+    await mongoose.connect(process.env.MONGODB_URI)
     .then(()=>console.log("DB connected"));
 }
